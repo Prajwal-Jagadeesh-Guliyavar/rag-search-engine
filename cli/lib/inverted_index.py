6929463a5e2d102717797025f9688560824314a0
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2025-12-02 12:32:14 +0530

    inverted_index.py file with the class InvertedIndex
    the class methods(functions) are:
    __add_document
    get_documents
    build
    save
    
    the models are saved in the path dir called dir in the name index,
    docmap in pickle format

---
+++ cli/lib/inverted_index.py
@@ -0,0 +1,43 @@
+import os
+import pickle
+from .search_utils import load_movies, tokenize_text, PROJECT_ROOT
+
+
+class InvertedIndex:
+    def __init__(self):
+        self.index = {}
+        self.docmap = {}
+
+    def __add_document(self, doc_id: int, text: str):
+        tokens = tokenize_text(text)
+        for token in tokens:
+            if token not in self.index:
+                self.index[token] = set()
+            self.index[token].add(doc_id)
+
+    def get_documents(self, term: str) -> list[int]:
+        term = term.lower()
+        if term not in self.index:
+            return []
+        
+        return sorted(list(self.index[term]))
+
+    def build(self):
+        movies = load_movies()
+        for movie in movies:
+            self.docmap[movie["id"]] = movie
+            text_to_index = f"{movie['title']} {movie['description']}"
+            self.__add_document(movie["id"], text_to_index)
+
+    def save(self):
+        cache_dir = os.path.join(PROJECT_ROOT, "cache")
+        if not os.path.exists(cache_dir):
+            os.makedirs(cache_dir)
+
+        index_path = os.path.join(cache_dir, "index.pkl")
+        with open(index_path, "wb") as f:
+            pickle.dump(self.index, f)
+
+        docmap_path = os.path.join(cache_dir, "docmap.pkl")
+        with open(docmap_path, "wb") as f:
+            pickle.dump(self.docmap, f)
